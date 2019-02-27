38da0b8dc2d822f05b985e7cc2c3c44b8aea5dc3
vinay murigadi <vinay4goud@gmail.com>
2019-02-27 14:11:30 +0530

    Add files via upload

--- jsmethod.py
+++ jsmethod.py
@@ -6,54 +6,60 @@ it has methods to convert sting format file data type as  well as data type to s
 
 """
 
-import json # importing json
+import json  # importing json
 
 d = '{"a":"vinay", "b":40}'  # string value
-e = {"a": "navin", "c":45}   # data type value
+e = {"a": "navin", "c": 45}  # data type value
 
-class j_son:
+
+class Json:
     # b = '{"a":"vinay", "b":40}'
 
     """
     calling the value
     """
+
     def __init__(self, a):
         self.a = a
 
         # self.y = y
 
-    def load_(self):
-
-        '''
+    def loads(self):
+        """
         writing function to convert string to data type
         :return: data type value
-        '''
-        #z = self.a
+        """
+        # z = self.a
         h = json.loads(self.a)
         f = json.loads(self.a)
-        #print(h)
+        # print(h)
         return h
+
     def dump_(self):
         """
         writing function to convert data type value to  string value
-        :return: sring value 
+        :return: string 
         """
 
         h = json.dumps(self.a)
-        return(h)
+        return h
 
 
-z = j_son(d)
-zz = j_son(e)
-print (z.load_())
+z = Json(d)
+zz = Json(e)
 
-print (zz.dump_())
-#print(j_son)
+k = z.loads()
+print(f'reads complete value of file {k}')
+print(f'reads  sing value of file : {k["a"]}')
+
+p = zz.dump_()
+print(f'reading  complete value of the file {p}')
+# print (f'reading  single value  of file {p}')
 
 # zz = z._loads()
 
 # a = {"a":"vinay", "b": 40}
-#print(f'{json.dumps(a)}')
+# print(f'{json.dumps(a)}')
 
 
 # print (z)
