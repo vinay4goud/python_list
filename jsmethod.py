294dcb2a80dc03535ae817efc48a723268da2928
vinay murigadi <vinay4goud@gmail.com>
2019-02-27 13:35:57 +0530

    Add files via upload

--- jsmethod.py
+++ jsmethod.py
@@ -1,23 +1,44 @@
-import json
+"""
+JSON is a syntax for storing and exchanging data.
 
-d = '{"a":"vinay", "b":40}'
-e = {"a": "navin", "c":45}
+it has methods to convert sting format file data type as  well as data type to string format
+
+
+"""
+
+import json # importing json
+
+d = '{"a":"vinay", "b":40}'  # string value
+e = {"a": "navin", "c":45}   # data type value
 
 class j_son:
-    b = '{"a":"vinay", "b":40}'
+    # b = '{"a":"vinay", "b":40}'
 
+    """
+    calling the value
+    """
     def __init__(self, a):
         self.a = a
 
         # self.y = y
 
     def load_(self):
+
+        '''
+        writing function to convert string to data type
+        :return: data type value
+        '''
         #z = self.a
         h = json.loads(self.a)
         f = json.loads(self.a)
         #print(h)
         return h
     def dump_(self):
+        """
+        writing function to convert data type value to  string value
+        :return: sring value 
+        """
+
         h = json.dumps(self.a)
         return(h)
 
