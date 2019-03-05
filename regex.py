537b2f3930446d714063283f7d6d358122d00c92
vinay murigadi <vinay4goud@gmail.com>
2019-03-05 15:05:13 +0530

    Add files via upload

--- regex.py
+++ regex.py
@@ -1,4 +1,9 @@
+"""
+reading a file , extracting date , name and content from the file
+"""
+
 import re
+import json
 
 class expresions:
     def __init__(self, filesinfo):
@@ -8,6 +13,11 @@ class expresions:
     def readfile(self):
         filez = open(self.fi, "r")
         reading = filez.read()
+        return reading
+
+    def readlinz(self):
+        filez = open(self.fi, "r")
+        reading = filez.readline()
         return reading
 
     def date(self):
@@ -20,18 +30,75 @@ class expresions:
     def time(self):
 
 
-        match = re.findall("\d{1}:\d{2} am|\d{1}:\d{2} pm", self.readfile())
+        match = re.findall("\d{2}:\d{2} am|\d{2}:\d{2} pm", self.readfile())
 
 
 
         return match
 
+
+    def name(self):
+
+        match = re.split("\-",self.readlinz())
+        smatch = match[1]
+        #filedictionary = json.loads(smatch)
+        divideinfo = re.split("\:",smatch)
+
+        return divideinfo
+
+    def dataf(self):
+
+        date = re.findall("\d{2}/\d{2}/\d{2}",self.readlinz())
+
+        time = re.findall("\d{1}:\d{2} am|\d{1}:\d{2} pm", self.readlinz())
+
+        match = re.split("\-", self.readlinz())
+        smatch = match[1]
+        # filedictionary = json.loads(smatch)
+        divideinfo = re.split("\:", smatch)
+
+
+        return "".join(date) , "".join(time) , divideinfo[0], divideinfo[1]
+
+
+    def gaol(self):
+
+        x = self.dataf()
+
+        return (x)
+
+    def extractinformation(self):
+        information_data = {}
+
+        information_data['date'] = self.gaol()[0]
+        information_data['time'] = self.gaol()[1]
+        information_data['name'] = self.gaol()[2]
+        information_data['content'] = self.gaol()[3]
+
+        return information_data
+
+
+
+
 wfile = expresions("Chat with.txt")
 
 datez = wfile.date()
 timez = wfile.time()
+name = wfile.name()
+dataf = wfile.dataf()
+gaol = wfile.gaol()
+infrom = wfile.extractinformation()
 
-print (f'{datez} \n {timez}')
+#print (f'{datez} \n {timez}')
+
+
+
+#print (f'{name}')
+
+#print (f'{dataf}')
+print (f'{gaol[3]}')
+
+print (f'{infrom}')
 
 
 
